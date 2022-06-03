e531e96e201e89868a074aa005d2115ab480d2ba
ChawMaMaPyone <chawmama.p.cnsa@gmail.com>
2022-06-03 16:01:53 +0630

    20220603

--- lesson02/Form1.cs
+++ lesson02/Form1.cs
@@ -27,7 +27,7 @@ namespace lesson02
         public FRM_Bumon()
         {
             InitializeComponent();
-           //Testing chaw
+           //20220603 Chaw
         }
         #region BT_Close_Click and BT_NewInput_Click
         private void BT_Close_Click(object sender, EventArgs e)
